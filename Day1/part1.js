234febb4a697944c95ddcfbeff9c8c5fad634e31
Aurelie Fomum <azam_aurelie@yahoo.com>
2021-12-02 15:31:00 -0300

    day1-part1

--- Day1/part1.js
+++ Day1/part1.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const depths = fs.readFileSync('./part1.txt', {encoding: 'utf-8'}).split('\n').filter(x => x).map(x => +x)
-//console.log(depths)
+
 let depthIncreases = depths.map((x,i,arr) => x < arr[i+1]).filter(el=> el)
 //console.log(depthIncreases)
 console.log(depthIncreases.length)
